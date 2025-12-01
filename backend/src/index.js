ec67ee94c649c3f0ed2ff2a14158ed37417b0e3a
Julen37 <julen.gouchault@gmail.com>
2025-12-01 16:00:01 +0100

    initialisation projet appli librairie

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+// importation du framework Express
+import express from "express";
+import "dotenv/config";
+
+// creation d'une instance de l'appli Express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("api/auth", authRoutes);
+
+// definition du port d'ecoute du serveur
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`) // message de confirmation lorsque le serveur est en cours d'excecution
+});
