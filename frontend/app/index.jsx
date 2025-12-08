a8b13405919310da5c3068505b850ec8c6fb71bc
Julen37 <julen.gouchault@gmail.com>
2025-12-08 11:46:13 +0100

    frontend / style + color app

---
+++ frontend/app/index.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+import {Image} from "expo-image";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>hello</Text>
+      <Image source={{ uri:""}}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title : {
+    color: "blue",
+  }
+})
