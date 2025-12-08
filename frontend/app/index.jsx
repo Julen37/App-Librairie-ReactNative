ba38d6e3cdd08a891513f86b147078df7eaedcb3
Julen37 <julen.gouchault@gmail.com>
2025-12-08 14:00:13 +0100

    frontend / fonts / debut login-signup screen

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
-import {Image} from "expo-image";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>hello</Text>
-      <Image source={{ uri:""}}/>
+      <Link href="./(auth)/">Login</Link>
+      <Link href="./(auth)/signup">Signup</Link>
     </View>
   );
 }
