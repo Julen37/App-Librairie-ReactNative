fdf82cb0c5550d0718b0c5184cb6f1ba09a7699d
Julen37 <julen.gouchault@gmail.com>
2025-12-12 11:07:35 +0100

    frontend / bar de navigation

--- frontend/app/(tabs)/_layout.jsx
+++ frontend/app/(tabs)/_layout.jsx
@@ -1,17 +1,56 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from "expo-router";
+import { Ionicons }from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function TabLayout() {
+
+  const insets = useSafeAreaInsets();
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        headerTitleStyle: {
+          color: COLORS.textPrimary,
+          fontWeight: "600",
+        },
+        headerShadowVisible: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.background,
+          borderTopWidth: 1,
+          borderTopColor: COLORS.border,
+          paddingTop: 5,
+          paddingBottom: insets.bottom,
+          height: 60 + insets.bottom,
+        },
       }}
     >
-      <Tabs.Screen name='index'/>
-      <Tabs.Screen name='create'/>
-      <Tabs.Screen name='profile'/>
+      <Tabs.Screen 
+        name='index'
+        options={{
+          title: "Acceuil",
+          tabBarIcon: ({color, size}) =>(<Ionicons name='home-outline' size={size} color={color}/>)
+        }}
+      />
+      <Tabs.Screen 
+        name='create'
+        options={{
+          title: "Créer",
+          tabBarIcon: ({color, size}) =>(<Ionicons name='add-circle-outline' size={size} color={color}/>)
+
+        }}
+      />
+      <Tabs.Screen 
+        name='profile'
+        options={{
+          title: "Profil",
+          tabBarIcon: ({color, size}) =>(<Ionicons name='person-outline' size={size} color={color}/>)
+        }}
+      />
     </Tabs>
   )
 }
