ec087df610f7dab090ff25de00508177443ec830
Julen37 <julen.gouchault@gmail.com>
2025-12-12 10:20:25 +0100

    update frontend landingpages

---
+++ frontend/app/(tabs)/_layout.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen name='index'/>
+      <Tabs.Screen name='create'/>
+      <Tabs.Screen name='profile'/>
+    </Tabs>
+  )
+}
