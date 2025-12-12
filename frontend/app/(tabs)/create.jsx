ca6297e1cf9c93e482e7e8407ba8a89a0de3f82c
Julen37 <julen.gouchault@gmail.com>
2025-12-12 11:37:27 +0100

    frontend / start create tabs

--- frontend/app/(tabs)/create.jsx
+++ frontend/app/(tabs)/create.jsx
@@ -1,10 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, KeyboardAvoidingView, ScrollView, Platform } from 'react-native'
+import React, { useState } from 'react'
+import { useRouter } from 'expo-router';
+import styles from '../../assets/styles/create.styles';
 
 export default function Create() {
+
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  const [rating, setRating] = useState(3);
+  const [image, setImage] = useState(null);
+  const [imageBase64, setImageBase64] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const pickImage = async () => {
+    
+  }
+  const handleSubmit = async () => {
+
+  }
+
   return (
-    <View>
-      <Text>Create</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1}}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        contentContainerStyle={styles.container}
+        style={styles.scrollViewStyle}
+      >
+        <View style={styles.card}>
+          {/* header */}
+          <View>
+            <Text style={styles.title}>Ajouter une recommandation</Text>
+            <Text style={styles.subtitle}>Partager vos favoris avec la communauté</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
